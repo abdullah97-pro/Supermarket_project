6f6591b4150c99667d5a71a4304fd494d976d4e8
abdullah97-pro <abdullahahmadi019@gmail.com>
2024-09-11 09:28:18 +0430

    configre webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'), // Serve from the src directory
+    },
+    compress: true,
+    port: 8080, // You can change the port if needed
+  },
+};
